301dc5c96e6db00052253cebc4a5caf1245ccf4d
edspohr <edmundo@spohr.cl>
2026-02-06 14:21:52 -0300

    fix(frontend): resolve postcss conflict & cleanup backend worker

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,19 +1,10 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
   server: {
     proxy: {
        // Proxy to Cloud Functions local emulator if needed
